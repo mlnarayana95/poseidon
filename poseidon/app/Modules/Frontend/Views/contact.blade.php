2c45121538574c09c67dcedbb97f8ef2faa676fc
evgheni <espko.21@gmail.com>
2019-11-28 11:37:08 -0600

    contact.php make changes emplent design

--- poseidon/app/Modules/Frontend/Views/contact.blade.php
+++ poseidon/app/Modules/Frontend/Views/contact.blade.php
@@ -2,24 +2,7 @@
 @section('title', 'Room List')
 
 @section('content')
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <!-- Boostrap CSS -->
-    <link rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous">
-    <!-- fontawesome -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <!-- fontawesome -->
-    <script src="https://kit.fontawesome.com/58d8ede058.js" crossorigin="anonymous"></script>
-    <sytle>
-        
-    </sytle>
+
 <div class="title-div">
     <div class="container">
         <div class="row content-header">
@@ -47,38 +30,42 @@
         </div>
     </div>
 </div>
+
 <div id="contact_form" class="container">
+
     <div class="row">
         <div class="col-md-6">
             <form>
+                  <span class="required_asterisk"></span>
               <div class="form-row">
                 <div class="form-group col-md-6">
-                  <label for="name">Name</label>
-                  <input type="text" class="form-control" id="name" placeholder="Name">
+                  <label for="name" class="required">Name</label>
+                  <input type="text" class="form-control" id="name" placeholder="Name" required>
                 </div>
                 <div class="form-group col-md-6">
-                  <label for="email">Email</label>
-                  <input type="email" class="form-control" id="email" placeholder="Email">
+                  <label for="email" class="required">Email</label>
+                  <input type="email" class="form-control" id="email" placeholder="Email" required>
                 </div>
               </div>
               <div class="form-group">
-                <label for="inputAddress">Subject</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="Subject">
+                <label for="inputAddress" class="required">Subject</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="Subject"
+                required>
               </div>
               <div class="form-group">
-                <label for="inputAddress2">Message</label>
+                <label for="inputAddress2" class="required">Message</label>
 
-                <textarea class="form-control"></textarea>
+                <textarea class="form-control" required></textarea>
               </div>
 
               <button type="submit" class="btn btn-danger">Send Message</button>
             </form>
         </div>
         <div class="col-md-6">
-            <div class="container">
+            <div class="icons">
             <p>
                 <a href="#" class="btn btn-info btn-lg">
-                    <span class="glyphicon glyphicon-map-marker"></span>
+                    <span class="fas fa-map-marker-alt"></span>
                     Fake Adress,Winnipeg<br/>
                          MB R7B 7T7
                 </a>
@@ -87,14 +74,14 @@
 
            <p>
             <a href="#" class="btn btn-info btn-lg">
-              <span class="glyphicon glyphicon-bullhorn"></span>
+              <span class="fas fa-phone-alt"></span>
                1(289)7777-777 
             </a>
            
           </p>
           <p>
             <a href="#" class="btn btn-info btn-lg">
-              <span class="glyphicon glyphicon-envelope"></span>
+              <span class="fas fa-envelope-open-text"></span>
               help@poseidon.com
             </a>
             
@@ -104,20 +91,7 @@
         </div>
     </div>
 </div>
-<link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.0/css/ion.rangeSlider.min.css"/>
 
-<!--jQuery khosada-->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
 <!-- Evgheni Code Ends Here -->
 
 @endsection
