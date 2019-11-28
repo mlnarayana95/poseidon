5e8145e339c9302b6bee461f89ded9bf9072f249
Krita <maharjan-k@webmail.uwinnipeg.ca>
2019-11-28 09:43:37 -0600

    Backend Layout Completed

---
+++ poseidon/resources/views/layouts/admin/main.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+@include('layouts.admin.partials.head')
+<body class="hold-transition fixed skin-blue sidebar-mini sidebar-mini-expand-feature">
+<div class="wrapper">
+    @include('layouts.admin.partials.head')
+    @include('layouts.admin.partials.header')
+    @include('layouts.admin.partials.content')
+    @include('layouts.admin.partials.footer')
+</div>
+</body>
+</html>
