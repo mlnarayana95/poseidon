13247fc31acdee6ab6f25f835c9469bb8c50de2a
juanjimenez80 <juancaji00@gmail.com>
2019-12-01 16:21:52 -0600

    login development finished

--- poseidon/app/Http/Controllers/Auth/LoginController.php
+++ poseidon/app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use http\Env\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -35,5 +36,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request){
+        $input = $request->all();
+        $this->validate($request,['email'=>'required|email','password'=>'required']);
+        if(auth()->attempt(array('email'=>$input['email'],'password'=>$input['password'])))
+        {
+            if(auth()->user()->user_type==1)
+            {
+                return redirect()->route('admin.home');
+            }
+            else{
+                return $this->redirect()->route('home');
+            }
+        }
+        else{
+
+            return redirect()->route('login')->with('error','Email-Address And Password are Wrong');
+        }
+
     }
 }
