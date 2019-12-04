593e8a0eca111a7e5592bee8b09600a4464beeb6
Krita <maharjan-k@webmail.uwinnipeg.ca>
2019-12-04 11:12:44 -0600

    Contact Us Page SetUp

--- poseidon/app/Modules/Frontend/Views/contact.blade.php
+++ poseidon/app/Modules/Frontend/Views/contact.blade.php
@@ -33,7 +33,7 @@
 
     <div class="row">
         <div class="col-md-6">
-            <form>
+            <form method="post">
                   <span class="required_asterisk"></span>
               <div class="form-row">
                 <div class="form-group col-md-6">
