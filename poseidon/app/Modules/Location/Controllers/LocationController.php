e4cffc8b344358f1adc98c091156743f106b14de
Narayana Madabhushi <mlnarayana95@gmail.com>
2019-12-03 22:54:09 -0600

    Modified the routes and controller

--- poseidon/app/Modules/Location/Controllers/LocationController.php
+++ poseidon/app/Modules/Location/Controllers/LocationController.php
@@ -15,7 +15,7 @@ class LocationController extends Controller
      */
     public function index()
     {
-        return view("Destination::index");
+        return view("Location::index");
     }
 
     /**
