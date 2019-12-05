45c237cc09c2048006a3dfd227e9a874abe9fcaf
Krita <maharjan-k@webmail.uwinnipeg.ca>
2019-12-05 10:55:26 -0600

    Hotel Soft Delete

--- poseidon/app/Modules/Hotel/Models/Hotel.php
+++ poseidon/app/Modules/Hotel/Models/Hotel.php
@@ -4,12 +4,21 @@ namespace App\Modules\Hotel\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Hotel extends Model {
 
     use Sluggable;
+    use SoftDeletes;
 
     protected $fillable = ['name', 'description', 'location_id', 'address', 'postal_code', 'phone_number', 'checkin_time', 'checkout_time', 'airport_distance', 'airport_transportation', 'pet_friendly'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
     /**
      * Return the sluggable configuration array for this model.
