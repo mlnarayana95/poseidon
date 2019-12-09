47390f1d64c63891152435c21bf70937b296b88d
Krita <maharjan-k@webmail.uwinnipeg.ca>
2019-12-09 12:47:24 -0600

    Amenity

--- poseidon/app/Modules/Amenity/Controllers/AmenityController.php
+++ poseidon/app/Modules/Amenity/Controllers/AmenityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Amenity\Controllers;
 
+use App\Modules\Feature\Models\Feature;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,7 +16,8 @@ class AmenityController extends Controller
      */
     public function index()
     {
-        return view("Amenity::index");
+        $data['features'] = Feature::get();
+        return view("Feature::index", $data);
     }
 
     /**
@@ -25,7 +27,7 @@ class AmenityController extends Controller
      */
     public function create()
     {
-        //
+        return view("Feature::add");
     }
 
     /**
@@ -36,18 +38,13 @@ class AmenityController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        // Validate Form Inputs
+        $validated_data = $this->validateFeature($request);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+        Feature::create($validated_data);
+
+        flash('Feature has been created successfully!')->success();
+        return redirect()->route('admin.feature.index');
     }
 
     /**
@@ -58,7 +55,8 @@ class AmenityController extends Controller
      */
     public function edit($id)
     {
-        //
+        $data['feature'] = Feature::find($id);
+        return view("Feature::edit", $data);
     }
 
     /**
@@ -70,7 +68,13 @@ class AmenityController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // Validate Form Inputs
+        $validated_data = $this->validateFeature($request);
+
+        Feature::find($id)->update($validated_data);
+
+        flash('Feature has been updated successfully!')->success();
+        return redirect()->route('admin.feature.index');
     }
 
     /**
@@ -81,6 +85,25 @@ class AmenityController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $feature = new Feature();
+        $feature->remove($id);
+        flash('Feature has been deleted successfully!')->success();
+        return redirect()->route('admin.feature.index');
+    }
+
+    /**
+     * Validate Feature Form
+     * @param $request
+     * @return mixed
+     */
+    public function validateFeature($request)
+    {
+        $rules = [
+            'feature' => 'required|max:255',
+            'feature_icon' => 'required|max:20'
+        ];
+
+        $validated_data = $request->validate($rules);
+        return $validated_data;
     }
 }
--- poseidon/app/Modules/Amenity/Views/index.blade.php
+++ poseidon/app/Modules/Amenity/Views/index.blade.php
@@ -1,3 +1,81 @@
-<?php
+@extends('layouts.admin.main')
+@section('title', 'Features')
+@section('heading', 'Features')
+@section('breadcrumb')
+    @parent
+    <li>
+        <a href="#">Features</a>
+    </li>
+@stop
 
-echo trans('Amenity::example.welcome');
+@section('content')
+    <div class="col-xs-12">
+        @include('flash::message')
+
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">All Features</h3>
+
+                <div class="box-tools">
+                    <a href="{{ route('admin.feature.create') }}"
+                       class="btn btn-primary">
+                        <i class="fa fa-plus-circle"></i> Add Feature
+                    </a>
+                </div>
+            </div>
+            <div class="box-body">
+
+                <table id="features" class="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th>Feature ID</th>
+                        <th>Feature</th>
+                        <th>Icon</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    @foreach($features as $feature)
+                        <tr>
+                            <td>{{ $feature->id }}</td>
+                            <td>{{ $feature->feature }}</td>
+                            <td>{{ $feature->feature_icon }}</td>
+                            <td>
+                                <a href="{{ route('admin.feature.edit', $feature) }}"
+                                   class="btn btn-primary marginRight">
+                                    <i class="fa fa-pencil"></i> Edit
+                                </a>
+
+
+                                <a href="{{ route('admin.feature.destroy', $feature) }}" data-method="delete"
+                                   data-token="{{csrf_token()}}"
+                                   data-confirm="Are you sure?" class="btn btn-danger">
+                                    <i class="fa fa-trash"></i> Delete
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(function () {
+            $('#features').DataTable({
+                'paging': true,
+                'lengthChange': true,
+                'searching': false,
+                'ordering': true,
+                'info': true,
+                'autoWidth': false,
+                "order": [[ 0, "desc" ]]
+            });
+        });
+    </script>
+@endsection
