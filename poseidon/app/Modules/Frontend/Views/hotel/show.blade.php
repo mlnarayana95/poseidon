08ed4e0ae7fccb91c6d8640982b48eaee38aeba2
peterskatie <katie.peters@hotmail.com>
2019-12-10 00:12:26 -0600

    show.blade is dynamic for hotels. Still need to add styles.

--- poseidon/app/Modules/Frontend/Views/hotel/show.blade.php
+++ poseidon/app/Modules/Frontend/Views/hotel/show.blade.php
@@ -42,6 +42,10 @@
       margin-top: 20px;
     }
     
+    #hotel-page div.info{
+      text-align: center;
+    }
+    
     .hotel-star.fas {
       color: #000;
       display: inline-block;
@@ -60,28 +64,79 @@
     .amenity-check.fas{
       color: #000;
     }
+    
+    #myCarousel{
+      max-width: 700px;
+      margin: 0 auto 30px;
+    }
+    
+    #hotel-page div.description{
+      margin-bottom: 30px;
+    }
 @endsection
 
 @section('content')
 
 <div id='hotel-page'>
-  <div class="container">
+  <div class="container info">
     <h1>{{ $hotel->name }}</h1>
     <div class="hotel-location">{{ $hotel->address}}</div>
+    <div class="hotel-phone">
+      <span><em>Phone number: </em>{{ $hotel->phone_number }}</span>
+    </div>
+    <div class="check-in-out">
+      <span><strong>Check in - </strong>{{ $hotel->checkin_time }}</span>
+      <br>
+      <span><strong>Check out - </strong>{{ $hotel->checkout_time }}</span>
+    </div>
+    <div class="hotel-distance">
+      <span><strong>Distance from Airport: </strong>{{ $hotel->airport_distance }} <em>with</em> <strong>Transportation Charge: </strong>{{ $hotel->airport_transportation }}</span>
+    </div>
+    <div class="check-in-out">
+      <span><strong>is pet friendly? </strong>{{ $hotel->pet_friendly ? 'yes' : 'no' }}</span>
+    </div>
   </div>
   
-  
+  <div class="container">
+    <div id="myCarousel" class="carousel slide"
+         data-ride="carousel">
+         <ol class="carousel-indicators">
+           <li data-taget="myCarousel" data-slide-to="0" class="active"></li>
+         </ol>
+        <div class="carousel-inner">
+            
+            @foreach($hotel->images as $key => $image)
+            <div class="carousel-item {{$key == 0 ? 'active' : ''}}">
+                <img src="/images/hotels/{{$image->file_name}}" class="d-block w-100"
+                     alt="{{$image->file_name}}">
+            </div>
+            @endforeach
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel"
+           role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon"
+                      aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel"
+           role="button" data-slide="next">
+                <span class="carousel-control-next-icon"
+                      aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+  </div>
   
   <div class="container">
-    <div class="row">
-      <div class="col-md-6">
+    <div class="row description">
+      <div class="col-md-9">
         <div class="about-hotel">
           Before You Go
         </div>
         <div class="hotel-description">{{ $hotel->description}}</div>
       </div>
       
-      <div class="col-md-6">
+      <div class="col-md-3">
         <div class="about-hotel">
           Amenities
         </div>
@@ -94,109 +149,5 @@
     </div>
   </div>
 </div> <!-- hotel-page div -->
-
-
-
-
-
-
-    <!--@foreach($hotel->images as $image)
-        <img src="/images/hotels/{{ $image->file_name }}" alt=""/>
-        @endforeach -- >
-
-    <div class="container py-2">
-        <div class="row min-vh-100 align-items-center">
-            <div class="col-lg-8 offset-lg-2" id="slider">
-                <div id="myCarousel" class="carousel slide shadow">
-                    <!-- main slider carousel items -->
-                    <div class="carousel-inner">
-                        <div class="active carousel-item" data-slide-number="0">
-                            <img src="http://placehold.it/1200x480&amp;text=one" class="img-fluid">
-                        </div>
-                        <div class="carousel-item" data-slide-number="1">
-                            <img src="http://placehold.it/1200x480/888/FFF" class="img-fluid">
-                        </div>
-                        <div class="carousel-item" data-slide-number="2">
-                            <img src="http://placehold.it/1200x480&amp;text=three" class="img-fluid">
-                        </div>
-                        <div class="carousel-item" data-slide-number="3">
-                            <img src="http://placehold.it/1200x480&amp;text=four" class="img-fluid">
-                        </div>
-                        <div class="carousel-item" data-slide-number="4">
-                            <img src="http://placehold.it/1200x480&amp;text=five" class="img-fluid">
-                        </div>
-                        <div class="carousel-item" data-slide-number="5">
-                            <img src="http://placehold.it/1200x480&amp;text=six" class="img-fluid">
-                        </div>
-                        <div class="carousel-item" data-slide-number="6">
-                            <img src="http://placehold.it/1200x480&amp;text=seven" class="img-fluid">
-                        </div>
-                        <div class="carousel-item" data-slide-number="7">
-                            <img src="http://placehold.it/1200x480&amp;text=eight" class="img-fluid">
-                        </div>
-
-                        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-
-                    </div>
-                    <!-- main slider carousel nav controls -->
-
-
-                    <ul class="carousel-indicators list-inline mx-auto border px-2">
-                        <li class="list-inline-item active">
-                            <a id="carousel-selector-0" class="selected" data-slide-to="0" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=one" class="img-fluid">
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a id="carousel-selector-1" data-slide-to="1" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=two" class="img-fluid">
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a id="carousel-selector-2" data-slide-to="2" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=three" class="img-fluid">
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a id="carousel-selector-3" data-slide-to="3" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=four" class="img-fluid">
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a id="carousel-selector-4" data-slide-to="4" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=five" class="img-fluid">
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a id="carousel-selector-5" data-slide-to="5" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=six" class="img-fluid">
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a id="carousel-selector-6" data-slide-to="6" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=seven" class="img-fluid">
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a id="carousel-selector-7" data-slide-to="7" data-target="#myCarousel">
-                                <img src="http://placehold.it/80x60&amp;text=eight" class="img-fluid">
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-        <!--/main slider carousel-->
-    </div>
-
-
 
 @endsection
