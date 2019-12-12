7d88a76a9cca05e1252ab4386405188f17b21f87
Narayana Madabhushi <mlnarayana95@gmail.com>
2019-12-12 12:35:11 -0600

    Added Hotel Image upload field

--- poseidon/app/Modules/Hotel/routes/web.php
+++ poseidon/app/Modules/Hotel/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['module' => 'Hotel', 'route' => 'admin.', 'middleware' => ['web','auth','admin'], 'prefix' => 'admin', 'namespace' => 'App\Modules\Hotel\Controllers'], function() {
+Route::group(['module' => 'Hotel', 'route' => 'admin.', 'middleware' => ['web','auth'], 'prefix' => 'admin', 'namespace' => 'App\Modules\Hotel\Controllers'], function() {
 
     Route::name('admin.')->group(function () {
         Route::resource('hotel', 'HotelController');
